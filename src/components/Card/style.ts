6b902f104d3e09fde69f1774fc27f165c21b50b9
gabriel quirino <gquirino448@gmail.com>
2023-08-02 13:38:09 -0300

    first

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -16,7 +16,7 @@ export const CardHead = styled.div<Props>`
   height: ${props => props.tipo === 'true' ? '217px' : '167px'};
   width: 100%;
   img {
-    width: 100%;
+    width: 90%;
     height: ${props => props.tipo === 'true' ? '217px' : '167px'};
     object-fit: cover;
   }
