d2f95d1aa18e4bb87915dfb1ba28e8667e104840
gabriel quirino <gquirino448@gmail.com>
2023-08-02 13:06:21 -0300

    first

--- src/containers/Header/style.ts
+++ src/containers/Header/style.ts
@@ -35,7 +35,7 @@ export const HeadTitle = styled.h1<Props>`
   color: ${ colors.colorDark };
   font-weight: 900;
   font-size: ${props => props.size === 'true' ? '36px' : '18px'};
-  max-width: 600px;
+  max-width: 640px;
 `
 
 export const HeadLink = styled(Link)`
